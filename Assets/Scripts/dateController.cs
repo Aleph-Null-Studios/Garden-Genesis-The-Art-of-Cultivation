da9498ebcc7684060a12b4eff7a8db0bc1ea82f2
Chino Beach <beach988@morrisville.edu>
2023-02-04 14:07:12 -0500

    dateControllerCleanedUpYearWorking

--- Assets/Scripts/dateController.cs
+++ Assets/Scripts/dateController.cs
@@ -7,50 +7,34 @@ using TMPro;
 public class dateController : MonoBehaviour
 {
     /*variables*/
-    //buttons
-    [SerializeField] Button dayJumpButton;  //when pressed increases day
-    [SerializeField] Button weekJumpButton;  //when pressed increases week
-    [SerializeField] Button monthJumpButton;  //when pressed increases month
-    [SerializeField] Button yearJumpButton;  //when pressed increases year
 
     //date
-    int curYear;            //which year it currently is 
-    int curMonth;           //which month it currently is
-    int curDay;             //which day it currently is 
+    int curYear;                //which year it currently is 
+    int curMonth;               //which month it currently is (of the year)
+    int curDay;                 //which day it currently is (of the month)
 
     //max time restraints
-    int numMonthsInYear = 18;   //Number of months in the year
-    int numWeeksInMonth = 3;    //Number of weeks in a month
-    int numDaysInWeek = 10;     //Number of days in the week
-    int numDaysInYear;          //Number of days in a year (defined in start)
-    int numDaysInMonth;         //Number of days in the month (defined in start)
-
-
-
-    //time relative
-    int curDayInWeek;       //which day of the week is it
-    int curWeekInMonth;     //which week of the month is it
-
+    int numMonthsInYear = 18;   //Number of months in each year
+    int numDaysInMonth = 30;    //Number of days in each month 
+    int numDaysInYear;          //Number of days in each (defined in start)
+    
     //total tracker
-    int totalDays;          //how many days have passed since start of game
+    int totalDays;             //how many days have passed since the start of the gane
 
     // Start is called before the first frame update
     void Start()
     {
         //start the day counter
-        totalDays = 0; //this starts at 0 because no days have passed. 
+        totalDays = 0; //at the begining no days have passed so this is 0
 
         //current date would be the very first day
-        curYear = 1;
+        curYear = 1;    
         curMonth = 1;
         curDay = 1;
-
-        curDayInWeek = 1;
-        curWeekInMonth = 1;
         
         //set variables
-        numDaysInYear = numMonthsInYear * numWeeksInMonth * numDaysInWeek;
-        numDaysInMonth = numWeeksInMonth * numDaysInWeek;
+        numDaysInYear = numMonthsInYear * numDaysInMonth;
+
     }//end of start method
 
     // Update is called once per frame
@@ -59,89 +43,27 @@ public class dateController : MonoBehaviour
         
     }//end of update method
 
-    //CheckEnd is called when time is increased
-    bool CheckEnd(string checkingEndOf)
+   
+    //Increase time is called when a button is pressed(onclick). The button will pass through the correct incrememnet of time to be changed. 
+    void IncreaseTime(int numDaysChanging)
     {
-        //if you have a valid search
-        if (checkingEndOf == "week" || checkingEndOf == "month" || checkingEndOf == "year")
-        {
-            //if what you are checking is currently above the maximun of what it could be
-            if ((checkingEndOf == "week" && curDayInWeek > numDaysInWeek) ||
-                (checkingEndOf == "month" && curWeekInMonth > numWeeksInMonth) ||
-                (checkingEndOf == "year" && curMonth > numMonthsInYear))
-            {
-                //it is the end of the date variable you are checking
-                return true;
-            }//end if past max range
+        //update the total number of days based on the amount of time added.(selected with button)
+        totalDays += numDaysChanging;
 
-            else
-            {
-                //the date is still in range
-                return false;
-            }//end else in range
-        }//end if checking week,month,year
-
-        //search invalid
-        else
-        {
-            Debug.Log("Invalid search condition");
-            return false;
-        }//end search invalid
-
-    }//end of check end method
-
-    //Increase time is called when a button is pressed 
-    void IncreaseTime(Button inputChanging)
-    {
-        /*varibales*/
-        int numDaysChanging = 0;
-
-        //search for a valid button being pressed
-        if (inputChanging == dayJumpButton || inputChanging == weekJumpButton || inputChanging == monthJumpButton || inputChanging == yearJumpButton)
-        {
-            //find out how many days are changing based on the button pressed.
-            if (inputChanging == dayJumpButton)
-            {
-                numDaysChanging = 1;
-            }//end day button
-            else if (inputChanging == weekJumpButton)
-            {
-                numDaysChanging = numDaysInWeek;
-            }//end week button
-            else if (inputChanging == monthJumpButton)
-            {
-                numDaysChanging = numDaysInMonth;
-            }//end month button
-            else if (inputChanging == yearJumpButton)
-            {
-                numDaysChanging = numMonthsInYear;
-            }//end year button
-
-            //change the days
-            totalDays += numDaysChanging;
-
-        }//end corrent button
-        else
-        {
-            Debug.Log("Time not effected");
-           
-        }//end else
+        //update the calander date
+        UpdateTime();
 
     }//end of increase time method
 
     //called when the time changes
-    void UpdateTime(int numDaysChanged)
+    void UpdateTime()
     {
-        /*variables*/
-        bool isEndOf;
-        int intWeekDayDif;
-
-    
+        /*find the current day,month and year. */
+        
+        //current year = starting year (1) + this rounded down: (total days that have passed/total days in a year)
+        curYear = 1 + Mathf.FloorToInt(totalDays / numDaysInYear);
+        
 
     }//end of method update time
 
-    void timeLeft()
-    {
-
-    }
 }//end of class 
