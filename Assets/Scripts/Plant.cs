8729ea1816fffd87648adce9d84b58bc51e4dcfb
Chino Beach <beach988@morrisville.edu>
2023-02-05 01:31:40 -0500

    Update Plant.cs

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -13,7 +13,6 @@ public class Plant : MonoBehaviour
     void Start()
     {
         if (plantData == null) {
-            Debug.Log("You fucked up");
             // Set default plant
             return;
         }
