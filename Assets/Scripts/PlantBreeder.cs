accd4be1b8ade0b63bdea8ba5eec31fc30c2003e
Chino Beach <beach988@morrisville.edu>
2023-02-05 10:49:38 -0500

    BreedingScript

--- Assets/Scripts/PlantBreeder.cs
+++ Assets/Scripts/PlantBreeder.cs
@@ -6,8 +6,10 @@ public class PlantBreeder : MonoBehaviour
 {
     #region Variables
     // Variables.
-    public Plant parent1;
-    public Plant parent2;
+    public PlantData parent1;
+    public PlantData parent2;
+    
+    PlantManager plantManager;
     #endregion
 
     #region Unity Methods
@@ -17,6 +19,9 @@ public class PlantBreeder : MonoBehaviour
         // Make sure nothing is in these slots
         parent1 = null;
         parent2 = null;
+
+        plantManager = GetComponent<PlantManager>();
+        //BreedPlant();
     }
 
     void Update()
@@ -35,15 +40,21 @@ public class PlantBreeder : MonoBehaviour
     // Public Methods.
     public void BreedPlant()
     {
-        if (parent1 == null || parent2 == null) return;
+        if ((parent1 == null || parent2 == null) || (parent1 == parent2) || (plantManager.userPlants.Count >= 14)) return; //cannot be null or the same
+        //figure out what to pass to the baby
+        Color newflowerColor = Color.Lerp(parent1.flowerColor, parent2.flowerColor, Random.Range(.3f, .7f));
+        Sprite newflowerSprite = parent1.flowerSprite;
+        Sprite stemSprite = parent1.stemSprite;
 
         // Calculate new flower breed.
-
-
+        PlantData babyPlant = new PlantData(newflowerColor, newflowerSprite, stemSprite, 0);
         // Add to plant manager.
-        
+        plantManager.userPlants.Add(babyPlant);
+        Debug.Log(babyPlant.flowerColor);
 
         // Set parents to null.
+        parent1 = null;
+        parent2 = null;
     }
     #endregion
 }
